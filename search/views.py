1e7001ab431a40d90d47eef3330c59e26b27ae1c
Somani-Harsh <somani.hrsh@gmail.com>
2016-06-10 18:30:43 +0530

    Update views.py

--- search/views.py
+++ search/views.py
@@ -35,25 +35,39 @@ def get_restaurants(latitude="28.6276", longitude="77.2784", establishment_type=
 		'user_key': 'a560615348198de6373564df0e3bd857',
 	}
 	
+
+   
 	r = requests.get(
 		url=url, headers=header, params=params)
 
 
 	restaurants = r.json()
+	
+	
+
 	# This puts the best_rated_restaurants in a list. Refer to JSON Response.
 	# restaurants_list = {'restaurants': restaurants['restaurants']}
+
 	return restaurants
 
+
+
 def get_location(location):
+
+
 	api_key = "AIzaSyAP-URITVI1c0RfktFoHX78imOimvcwBps"
 	api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(location, api_key))
 	api_response_dict = api_response.json()
+
 
 	if api_response_dict['status'] == 'OK':
 		latitude = api_response_dict['results'][0]['geometry']['location']['lat']
 		longitude = api_response_dict['results'][0]['geometry']['location']['lng']
 
+	
 	return latitude, longitude
+
+
 
 def home(request):
 	try:
@@ -66,13 +80,22 @@ def home(request):
 		a =get_restaurants(lat,lon)
 		custom = []
 
-		count = 0 
-	
+		count = 0
+
 		for restaurant in a['restaurants']:
 			restaurant['restaurant']['review'] = get_reviews_details(restaurant['restaurant']['url'].split("?")[0])
-				
+			rlat = float(restaurant['restaurant']['location']['latitude'])
+			rlong = float(restaurant['restaurant']['location']['longitude'])
+
+			
+			restaurant['restaurant']['distance'] = str(haversine(lon, lat, rlong, rlat))+' km'
+
+
+
+
 
 		restaurants = json_normalize( a['restaurants'])
+		# print restaurants
 		return HttpResponse(restaurants.to_html())
 		# print restaurants
 
