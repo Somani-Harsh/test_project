61e987db749bbc05d605ddb2313baf1c26b0100e
Somani-Harsh <somani.hrsh@gmail.com>
2016-06-10 19:37:08 +0530

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -57,35 +57,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd56u1h2034l6ht',
-        'USER': 'wpdrxvdzyyxann',
-        'PASSWORD': 'FlAgFQ2sd64bUfqkeomj2ddiqr',
-        'HOST': 'ec2-54-83-202-115.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-Remember that these files import from the settings/base.py file, in which we defined the get_env_variable function as:
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
